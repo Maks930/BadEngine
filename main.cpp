9dbdc7fedee06b0e2d2bd4bd6da4516f3590dc3f
Maks <maksdemo80@gmail.com>
2025-06-28 21:01:14 +0300

    add glfw template

--- main.cpp
+++ main.cpp
@@ -1,7 +1,30 @@
 #include <iostream>
 
+#include <GLFW/glfw3.h>
+
 int main()
 {
+    if (!glfwInit()) {
+        std::cerr << "GLFW not init\n";
+        return -1;
+    }
 
+    GLFWwindow *win = glfwCreateWindow(640,480,"Test Window", nullptr, nullptr);
+    if (!win) {
+        glfwTerminate();
+        std::cerr << "Failed init window\n";
+        return -1;
+    }
+
+    glfwMakeContextCurrent(win);
+
+    while (!glfwWindowShouldClose(win)) {
+        // glClear(GL_COLOR_BUFFER_BIT);
+
+        glfwSwapBuffers(win);
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
     return 0;
 }
